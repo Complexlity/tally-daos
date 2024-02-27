e44fead9c9cc4e591cf96203bcdb5549820cbbc3
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-02-27 17:53:04 +0100

    feat: update route with active proposals

--- src/app/explore/route.ts
+++ src/app/explore/route.ts
@@ -1,39 +1,51 @@
+import { getActiveProposals } from "@/utils/getActiveProposals";
 import { getActiveProposalOrganizations } from "@/utils/getActiveProposalsOrganizations";
-import { Organization, PickedOrgs } from "@/utils/types";
-import { Frame } from "frames.js";
 import { getFrameHtml } from "@/utils/getFrameHtmlWithState";
+import { PickedOrgs } from "@/utils/types";
+import { Frame } from "frames.js";
 import { NextRequest, NextResponse } from "next/server";
-import dummyFrameJson from "@/utils/dummyFrameBody.json";
 
 // Opt out of caching for all data requests in the route segment
 type State = PickedOrgs;
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  console.log({ body });
   const { searchParams } = new URL(request.url);
-  const page = searchParams.get("page") ?? "explore";
+  const page = searchParams.get("page")
+  console.log({page})
   const {
-    untrustedData: { buttonIndex, state: serializedState },
+    untrustedData: { buttonIndex, state: serializedState, inputText },
   } = body;
 
   // const {
   //   //@ts-expect-error
   //   untrustedData: { buttonIndex, state: serializedState },
   // } = dummyFrameJson;
+  let state: State
+  if (!serializedState) {
+    console.log("State not found")
+  state = await getActiveProposalOrganizations() as unknown as State;
+  }
+  else {
 
-  console.time("State");
-  let state: State = serializedState
-    ? JSON.parse(decodeURIComponent(serializedState))
-    : await getActiveProposalOrganizations();
-  console.timeEnd("State");
+    state = JSON.parse(decodeURIComponent(serializedState)) as unknown as State
+    console.log("State found")
+  }
 
-  const startImageUrl = `${process.env.HOST}/images/explore`;
-  const nextState = `${encodeURIComponent(JSON.stringify(state))}`;
-  console.log(nextState.length);
 
-  const initialFrame: Frame & { state: any } = {
-    image: startImageUrl,
+
+  let imageUrl: string
+  let returnedFrame: Frame & {state? : any}
+
+  if (page === "explore") {
+    console.log("initial state")
+    console.log(state)
+    imageUrl = `${process.env.HOST}/images/explore?page=review`;
+    const nextState = `${encodeURIComponent(JSON.stringify(state))}`;
+
+
+     returnedFrame = {
+    image: imageUrl,
     version: "vNext",
     buttons: [
       {
@@ -42,12 +54,48 @@ export async function POST(request: NextRequest) {
       },
     ],
     inputText: "Enter chain number e.g 1, 2, 5",
-    postUrl: `${process.env.HOST}/explore`,
-    ogImage: startImageUrl,
+    postUrl: `${process.env.HOST}/explore?page=review`,
+    ogImage: imageUrl,
     state: nextState,
-  };
+    };
 
-  return new NextResponse(getFrameHtml(initialFrame), {
+
+
+  }
+
+  else if (page === "review") {
+    console.log("I am review")
+    const inputTextNumber = Number(inputText)
+    console.log({inputTextNumber})
+    if (isNaN(inputTextNumber) || inputTextNumber <= 0 || inputTextNumber > state.length) {
+      console.log("Wrong input")
+      return new NextResponse('Wrong input', {
+       status: 400
+     })
+    }
+    console.log("review State")
+    console.log(state)
+    console.log(state)
+    const current = state[inputTextNumber - 1]
+    console.log({ current })
+    let chainId = current.governances[0].id.split(':').slice(0, 2).join(":")
+    console.log({chainId})
+
+    const currentGovernanceIds = current.governances.map(gov => {
+
+     return gov.id
+    })
+
+    console.log({currentGovernanceIds})
+
+    const activeProposals = await getActiveProposals(chainId, currentGovernanceIds)
+    console.log({ activeProposals })
+  }
+
+
+
+
+  return new NextResponse(getFrameHtml(returnedFrame), {
     status: 200,
     headers: { "content-type": "text/html" },
   });
