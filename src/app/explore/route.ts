668a02773a24a578aa66b683cb65102f188adf3b
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-02-27 12:22:32 +0100

    feat: clone get frame html. Add frame state

--- src/app/explore/route.ts
+++ src/app/explore/route.ts
@@ -1,16 +1,47 @@
-import { Frame, getFrameHtml } from "frames.js";
+import { getActiveProposalOrganizations } from "@/utils/getActiveProposals";
+import { Organization } from "@/utils/types";
+import { Frame } from "frames.js";
+import { getFrameHtml } from "@/utils/getFrameHtmlWithState";
 import { NextRequest, NextResponse } from "next/server";
+import dummyFrameJson from '@/utils/dummyFrameBody.json'
 
 // Opt out of caching for all data requests in the route segment
-
+type State = {
+  page: 'explore' | 'review',
+  items: Organization[]
+}
 
 
 export async function POST(request: NextRequest) {
+  const {
+    untrustedData: { buttonIndex, state: serializedState },
+  } = await request.json();
+
+  // const {
+  //   //@ts-expect-error
+  //   untrustedData: { buttonIndex, state: serializedState },
+  // } = dummyFrameJson;
+
+  let state: State
+
+  if(!serializedState){
+    const items = await getActiveProposalOrganizations()
+    state = {
+      page: "explore",
+      items
+    }
+  }
+  else {
+    state = serializedState
+  }
+
 
 
 const startImageUrl = `${process.env.HOST}/images/explore`;
+  const nextState = `${encodeURIComponent(JSON.stringify(state))}`
+  console.log(nextState.length)
 
-const initialFrame: Frame = {
+const initialFrame: Frame & {state: any} = {
   image: startImageUrl,
   version: "vNext",
   buttons: [
@@ -22,7 +53,8 @@ const initialFrame: Frame = {
   inputText: "Enter chain number e.g 1, 2, 5",
   postUrl: `${process.env.HOST}/explore`,
   ogImage: startImageUrl,
-};
+  state: nextState
+}
 
 
 
