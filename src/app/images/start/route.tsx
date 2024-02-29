23a61e229114261de5ea18d3dd4ad2627f75b1a5
Complexlity <105590967+Complexlity@users.noreply.github.com>
2024-02-29 10:27:38 +0100

    feat: complete start image

--- src/app/images/start/route.tsx
+++ src/app/images/start/route.tsx
@@ -1,3 +1,5 @@
+import { getActiveProposals } from "@/utils/getActiveProposals";
+import { getActiveProposalOrganizations } from "@/utils/getActiveProposalsOrganizations";
 import { ImageResponse } from "next/og";
 // App router includes @vercel/og.
 // No need to install it.
@@ -10,17 +12,44 @@ export async function GET() {
       <div
         style={{
           fontSize: 40,
+          display: "flex",
           color: "black",
           background: "white",
           width: "100%",
           height: "100%",
-          padding: "50px 200px",
+          padding: "50px 5px",
           textAlign: "center",
           justifyContent: "center",
+          backgroundImage: "linear-gradient(to bottom, #fff, #d1a7e2)",
           alignItems: "center",
+          flexDirection: "column",
         }}
       >
-        Explore all open proposals
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "10px",
+            fontWeight: "bold",
+            fontSize: "60px",
+          }}
+        >
+          <span
+            style={{
+              backgroundImage:
+                "linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))",
+              backgroundClip: "text",
+              //@ts-ignore
+              "-webkit-background-clip": "text",
+              color: "transparent",
+            }}
+          >
+            Explore
+          </span>
+          <span>Open DAO proposals on</span>
+          <img src={`${process.env.HOST}/tally.png`} width={210} height={70} />
+        </div>
+        <p>Make Your Vote Count!</p>
       </div>
     ),
     {
